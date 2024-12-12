6ba4dde2963f8f5f62bd7ccba928ea6393d43ad5
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-12-12 09:48:08 -0300

    cors

--- src/main/java/com/artxp/artxp/security/cors/corsConfig.java
+++ src/main/java/com/artxp/artxp/security/cors/corsConfig.java
@@ -1,16 +1,25 @@
+package com.artxp.artxp.security.cors;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class corsConfig implements WebMvcConfigurer {
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+
+
         registry.addMapping("/**") // Permite el mapeo CORS para todas las rutas.
                 .allowedOrigins(
                         "http://localhost:5173",
                         "https://dh-py-fe-trial.vercel.app/")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite solo estos métodos HTTP.
                 .allowedHeaders("Authorization",
                         "Accept",
