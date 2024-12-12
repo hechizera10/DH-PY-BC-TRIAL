469b76b60705b3f1abd56995356beb1403b07ea3
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-12-12 09:37:01 -0300

    cors

--- src/main/java/com/artxp/artxp/security/cors/corsConfig.java
+++ src/main/java/com/artxp/artxp/security/cors/corsConfig.java
@@ -1,29 +1,15 @@
-package com.artxp.artxp.security.cors;//package com.artxp.artxp.security.cors;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.stream.Stream;
-
 @Configuration
 public class corsConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed.origins}")
-    private String[] allowedOrigins;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Combina el localhost con las URLs configuradas dinámicamente
-        String[] combinedOrigins = Stream.concat(
-                Stream.of("http://localhost:5173"), // Siempre incluye localhost
-                Stream.of(allowedOrigins)          // Añade las URLs dinámicas
-        ).toArray(String[]::new);
-
-        registry.addMapping("/**")
-                .allowedOrigins(combinedOrigins) // Usa el array combinado
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        registry.addMapping("/**") // Permite el mapeo CORS para todas las rutas.
+                .allowedOrigins("http://localhost:5173", "https://dh-py-fe-trial-ne1xmkkhh-maria-elena-ponces-projects.vercel.app") // Permite solo esta URL de origen.
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite solo estos métodos HTTP.
                 .allowedHeaders("Authorization",
                         "Accept",
                         "Cache-Control",
@@ -31,6 +17,6 @@ public class corsConfig implements WebMvcConfigurer {
                         "Origin",
                         "x-csrf-token",
                         "x-requested-with")
-                .allowCredentials(true);
+                .allowCredentials(true); // Permite el envío de credenciales (cookies, encabezados de autenticación, etc.).
     }
 }
